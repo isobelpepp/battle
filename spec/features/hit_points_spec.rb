3f173078995dd0e6f17ad789b8f0da80b9115299
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-07-14 15:38:20 +0100

    See player 1 hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,8 @@ feature 'View hit points' do
     sign_in_and_play
     expect(page).to have_content 'Rob: 60HP'
   end
+  scenario 'see player one hit points' do
+    sign_in_and_play
+    expect(page). to have_content 'Bob: 60HP'
+  end
 end
