17cfffd446fac60ca1f65e2afaae2296abb008cf
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-07-14 12:12:45 +0100

    Add web helper

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see player two hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Bob'
-    fill_in :player_2_name, with: 'Rob'
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Rob(60 hit points)'
   end
 end
